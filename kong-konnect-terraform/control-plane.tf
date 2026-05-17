943e43a8a90c7c211c7f794f35499a8bb8a870b2
sureshmadapathi <sureshmadapathii@gmail.com>
2026-05-17 15:29:53 +0400

    Import existing control plane into Terraform state

--- kong-konnect-terraform/control-plane.tf
+++ kong-konnect-terraform/control-plane.tf
@@ -1,3 +1,23 @@
+import {
+  to = konnect_gateway_control_plane.main
+  id = "7dd63da8-40c4-46ad-9ace-9d89709c712b"
+}
+
+resource "konnect_gateway_control_plane" "main" {
+  name         = "demo-control-plane"
+  description  = "Main control plane"
+  cluster_type = "CLUSTER_TYPE_HYBRID"
+  auth_type    = "pki_client_certs"
+
+  proxy_urls = [
+    {
+      host     = "localhost"
+      port     = 80
+      protocol = "http"
+    }
+  ]
+}
+
 resource "tls_private_key" "cluster" {
   algorithm = "RSA"
   rsa_bits  = 2048
@@ -21,4 +41,9 @@ resource "tls_self_signed_cert" "cluster" {
   lifecycle {
     ignore_changes = all
   }
+}
+
+resource "konnect_gateway_data_plane_client_certificate" "cluster" {
+  cert             = tls_self_signed_cert.cluster.cert_pem
+  control_plane_id = konnect_gateway_control_plane.main.id
 }
