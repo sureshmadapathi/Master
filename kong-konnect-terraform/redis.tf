51b181982d827f0c581fa2c9beab8ab26b26779c
sureshmadapathi <sureshmadapathii@gmail.com>
2026-05-17 12:06:16 +0400

    added few configs

--- kong-konnect-terraform/redis.tf
+++ kong-konnect-terraform/redis.tf
@@ -1,20 +1,20 @@
 resource "konnect_gateway_partial" "aws_valkey_redis" {
-  control_plane_id = var.control_plane_id
+  control_plane_id = konnect_gateway_control_plane.main.id    # ← reference resource directly
 
-  redis_ee = {
-    name = "aws-valkey-redis"
-    config = {
-      host                  = var.redis_host
-      port                  = var.redis_port
-      ssl                   = true
-      ssl_verify            = false
-      database              = 0
-      connect_timeout       = 2000
-      read_timeout          = 2000
-      send_timeout          = 2000
-      keepalive_pool_size   = 256
-      keepalive_backlog     = 0
-      connection_is_proxied = false
-    }
+  type = "redis-ee"
+  name = "aws-valkey-redis"
+
+  config = {
+    host                  = var.redis_host
+    port                  = var.redis_port
+    ssl                   = true
+    ssl_verify            = false
+    database              = 0
+    connect_timeout       = 2000
+    read_timeout          = 2000
+    send_timeout          = 2000
+    keepalive_pool_size   = 256
+    keepalive_backlog     = 0
+    connection_is_proxied = false
   }
 }
--- kong-konnect-terraform/vault.tf
+++ kong-konnect-terraform/vault.tf
@@ -1,12 +1,9 @@
-# Vault already created in previous pipeline run
-# Commenting out to avoid duplicate creation error
-
-# resource "konnect_gateway_vault" "aws_secrets" {
-#   control_plane_id = var.control_plane_id
-#   name             = "aws"
-#   prefix           = "aws-vault"
-#   description      = "AWS Secrets Manager Vault"
-#   config = jsonencode({
-#     region = var.aws_region
-#   })
-# }
+resource "konnect_gateway_vault" "aws_secrets" {
+  control_plane_id = konnect_gateway_control_plane.main.id    # ← reference resource directly
+  name             = "aws"
+  prefix           = "aws-vault"
+  description      = "AWS Secrets Manager Vault"
+  config = jsonencode({
+    region = var.aws_region
+  })
+}
